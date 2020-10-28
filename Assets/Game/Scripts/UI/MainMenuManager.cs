1c696ad03ed43341e444a45410752b8fdbcc3286
joesibarani2000 <joesibarani2000@gmail.com>
2020-10-28 18:05:06 +0700

    loading main menu to game scene

---
+++ Assets/Game/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TomWill;
+public class MainMenuManager : MonoBehaviour
+{
+    private void Start()
+    {
+        TWTransition.FadeOut();
+    }
+    public void PlayGame()
+    {
+        TWTransition.FadeIn(() => TWLoading.LoadScene("SampleScene"));
+    }
+
+    public void Keluar()
+    {
+        Application.Quit();
+    }
+}
+
